94bc25c74bf96f50e51e28aae969f284c75ca763
JuanSMarin2 <160736940+JuanSMarin2@users.noreply.github.com>
2026-03-21 14:35:11 -0500

    Implementacion de logica completa minijuego foto

--- Assets/Juan/Scripts/Minijuegos/Photo/SunCollider.cs
+++ Assets/Juan/Scripts/Minijuegos/Photo/SunCollider.cs
@@ -7,13 +7,14 @@ public class SunCollider : MonoBehaviour
  private void OnTriggerEnter2D(Collider2D other)
     {
       
+        Debug.Log("Colision con: " + other.gameObject.name);
           isBlocked = true;
         
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
-       
+         Debug.Log("Colision con: " + other.gameObject.name);
             isBlocked = false;
         
     }
