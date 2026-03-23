0aea4c63ba82cad8c5e8844fb5e2aac307756222
JuanSMarin2 <160736940+JuanSMarin2@users.noreply.github.com>
2026-03-22 20:36:49 -0500

    Primera implementacion arte juego foto

--- Assets/Juan/Scripts/Minijuegos/Photo/SunCollider.cs
+++ Assets/Juan/Scripts/Minijuegos/Photo/SunCollider.cs
@@ -1,21 +1,61 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class SunCollider : MonoBehaviour
 {
 
     public bool isBlocked = false;
+    private readonly HashSet<Collider2D> collidersInside = new HashSet<Collider2D>();
+    private readonly Dictionary<PhotoParticleActiver, int> activersInside = new Dictionary<PhotoParticleActiver, int>();
+
  private void OnTriggerEnter2D(Collider2D other)
     {
-      
-        Debug.Log("Colision con: " + other.gameObject.name);
-          isBlocked = true;
+        if (other == null)
+            return;
+
+        collidersInside.Add(other);
+        isBlocked = collidersInside.Count > 0;
+
+        PhotoParticleActiver activer = other.GetComponentInParent<PhotoParticleActiver>();
+        if (activer != null)
+        {
+            if (!activersInside.ContainsKey(activer))
+                activersInside[activer] = 0;
+
+            activersInside[activer]++;
+        }
         
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
-         Debug.Log("Colision con: " + other.gameObject.name);
-            isBlocked = false;
+        if (other == null)
+            return;
+
+        collidersInside.Remove(other);
+        isBlocked = collidersInside.Count > 0;
+
+        PhotoParticleActiver activer = other.GetComponentInParent<PhotoParticleActiver>();
+        if (activer != null && activersInside.TryGetValue(activer, out int count))
+        {
+            count--;
+            if (count <= 0)
+                activersInside.Remove(activer);
+            else
+                activersInside[activer] = count;
+        }
         
+    }
+
+    public void ActivateAngryParticlesInside()
+    {
+        foreach (KeyValuePair<PhotoParticleActiver, int> entry in activersInside)
+        {
+            PhotoParticleActiver activer = entry.Key;
+            if (activer == null)
+                continue;
+
+            activer.ActivateAngryParticle();
+        }
     }
 }
